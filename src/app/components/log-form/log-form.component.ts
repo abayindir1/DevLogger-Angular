2cbfada64077b01e10577f80450077119b2a2c77
Abdullah Bayindir <byndrabdullah@gmail.com>
2019-12-30 21:27:47 -0600

    add log and update log functionality

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Log} from '../../models/log';
+import {LogService} from '../../services/log.service';
 
 @Component({
   selector: 'app-log-form',
@@ -6,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-form.component.css']
 })
 export class LogFormComponent implements OnInit {
+  id:string;
+  text:string;
+  date:any;
+  isNew:boolean= true;
 
-  constructor() { }
+  constructor(private logService: LogService) { }
 
   ngOnInit() {
+    //subscribe to the selectedLog observable
+    this.logService.selectedLog.subscribe(log=>{
+      if(log.id !== null){
+        this.isNew = false
+        this.id = log.id;
+        this.text = log.text;
+        this.date = log.date
+      }
+    })
+  }
+
+  onSubmit(){
+    if(this.isNew){
+      const newLog = {
+        id:this.generateId(),
+        text:this.text,
+        date: new Date()
+      }
+      this.logService.addLog(newLog);
+    }else{
+      const updateLog={
+        id:this.id,
+        text:this.text,
+        date: new Date()
+      }
+      this.logService.updLog(updateLog);
+
+    }
+  }
+
+  generateId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
   }
 
 }
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,27 +1,69 @@
-import { Injectable } from '@angular/core';
-import {Log} from '../models/log'
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+import { Observable } from "rxjs";
+import { of } from "rxjs";
+
+import { Log } from "../models/log";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class LogService {
-  logs:Log[];
+  logs: Log[];
+  private logSource = new BehaviorSubject<Log>({
+    id: null,
+    text: null,
+    date: null
+  });
+  selectedLog = this.logSource.asObservable();
 
-  constructor() { 
-    this.logs=[
+  constructor() {
+    this.logs = [
       {
-        id:'1', text:'Generated Components', date: new Date('12/28/2019 12:54:31')
+        id: "1",
+        text: "Generated Components",
+        date: new Date("12/28/2019 12:54:31")
       },
       {
-        id:'2', text:'Added Bootstrap', date: new Date('12/27/2019 10:52:31')
+        id: "2",
+        text: "Added Bootstrap",
+        date: new Date("12/27/2019 10:52:31")
       },
       {
-        id:'3', text:'Added Font Awesome', date: new Date('12/29/2019 11:14:27')
+        id: "3",
+        text: "Added Font Awesome",
+        date: new Date("12/29/2019 11:14:27")
       }
-    ]
-   }
+    ];
+  }
 
-   getLogs(){
-     return this.logs
-   }
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
+  }
+
+  addLog(log: Log) {
+    this.logs.unshift(log);
+  }
+
+  updLog(log: Log) {
+    this.logs.forEach((cur, i) => {
+      if (log.id === cur.id) {
+        this.logs.splice(i, 1);
+      }
+    });
+    this.logs.unshift(log);
+  }
+
+  deleteLog(log: Log) {
+    this.logs.forEach((cur, i) => {
+      if (log.id === cur.id) {
+        this.logs.splice(i, 1);
+      }
+    });
+  }
 }
